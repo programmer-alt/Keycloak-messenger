8737ed9a0ef8fa6ca22d076dc5d501eff429ba95
Vadim <modstils88@mail.ru>
2025-06-04 18:25:07 +0300

    Add Keycloak auth and session management to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import Keycloak from 'keycloak-connect';
+import session from 'express-session';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+// настройка сессий
+const memoryStore = new session.MemoryStore();
 
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUnitilized: true,
+    store: memoryStore,
+}),
+);
+// инициализация Keycloak
+const keycloak = new Keycloak({
+  store: memoryStore(),
+});
+app.use(keycloak.middleware());
 app.listen(PORT, ()=> {
     console.log(` Сервер работает на порту ${PORT}`);
 });
