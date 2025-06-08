c282c93056a52423b550843fa3b0b2332563d6cc
Vadim <modstils88@mail.ru>
2025-06-08 16:10:40 +0300

    Add Keycloak configuration for authentication

---
+++ server/src/keycloak/keycloakConfig.ts
@@ -0,0 +1,8 @@
+ export const keycloakConfig = {
+        'realm': 'Messenger',
+        'auth-server-url': 'http://localhost:8080/auth',
+        'ssl-required': 'external',
+        'resource': 'Messenger-realm',
+        'public-client': true,
+        'confidential-port': 0,
+    };
