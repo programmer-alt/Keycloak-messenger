6ea4d1f33752f070e78b75f5427643cc7756a74c
Vadim <modstils88@mail.ru>
2025-06-08 16:09:57 +0300

    Add Keycloak initialization and session setup

---
+++ server/src/keycloak/initializeKeycloak.ts
@@ -0,0 +1,27 @@
+import Keycloak from 'keycloak-connect';
+import session from 'express-session';
+import express from 'express';
+import { keycloakConfig } from './keycloakConfig';
+
+export function initializeKeycloak() {
+    const memoryStore = new session.MemoryStore();
+    const app = express();
+
+    app.use(session({
+        secret: 'some secret',
+        resave: false,
+        saveUninitialized: true,
+        store: memoryStore,
+    }));
+
+   
+
+    const keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+
+    app.use(keycloak.middleware({
+        logout: '/logout',
+        admin: '/admin',
+    }));
+
+    return keycloak;
+}
