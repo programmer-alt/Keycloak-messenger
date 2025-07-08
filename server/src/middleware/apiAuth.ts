bb0d0fb2241a7a0ad7f8ee940567945e5650e4aa
Vadim <modstils88@mail.ru>
2025-07-08 16:25:23 +0300

    Add API authentication middleware with Keycloak integration

---
+++ server/src/middleware/apiAuth.ts
@@ -0,0 +1,35 @@
+
+import { NextFunction, Request, Response } from "express";
+import  Keycloak, {Grant}  from "keycloak-connect";
+
+// 1. Определяем интерфейс, который расширяет стандартный Request
+interface RequestWithKauth extends Request {
+  kauth?: {
+    grant?: Grant;
+  };
+}
+/**
+ * protectApi — это функция, которая создаёт middleware для Express.
+Этот middleware:
+Проверяет, авторизован ли пользователь через Keycloak.
+Если не авторизован — возвращает ошибку 401.
+Если авторизован — даёт доступ к API.
+ */
+export const protectApi = (keycloack: Keycloak.Keycloak ) => {
+    const protector = keycloack.protect();
+    return (req: Request, res: Response, next: NextFunction) => {
+        protector (req, res, (err)=> {
+            const reqWithKauth = req as RequestWithKauth;
+            if ('kauth' in reqWithKauth){
+
+                const {kauth} = reqWithKauth;
+                console.log('kauth:', kauth);
+            }
+            if (err || !reqWithKauth.kauth?.grant){
+                return res.status(401).json({error: ' Не авторизовано', message:' Необходима аутентификация для доступа к этому ресурсу'});
+
+            }
+            return next()
+  ;      });
+    };
+}
